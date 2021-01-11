dc7b6f9f26b9042e750fcb148b8fd8755dfece17
w3b3ye <w3b3ye@gmail.com>
2021-01-11 21:00:22 +0400

    Reading XML using findall

--- xmlexmpl0.py
+++ xmlexmpl0.py
@@ -4,19 +4,26 @@ mydoc = ET.parse('XML/sample0.xml')
 myroot = mydoc.getroot()
 
 #Print elements startting from root  
-print("Elements startting from root")
+print("***Elements startting from root***")
 print(myroot)
 print(myroot[0])
 print(myroot[0][0])
 print("\n")
 #Print value of an elements
-print("Value of an elements")  
+print("***Value of an elements***")  
 print(myroot[0][0].text)
 print("\n")
 #Print all child elements in a root
-print("All child elements in a root")
+print("***All child elements in a root***")
 for c in myroot[0]:
     print(c.text)
+#Print all name in food using findall 
+print("***All name in food using findall***")
+for x in myroot.findall("food"):
+    name=x.find("name").text
+    price=x.find("price").text
+    print(name,price)
+
 
 
 
