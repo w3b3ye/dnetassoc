5f7994457a504414e53e2a8bee083779ec13eb75
w3b3ye <w3b3ye@gmail.com>
2021-01-12 21:30:24 +0400

    Modifying XML doc

--- xmlexmpl0.py
+++ xmlexmpl0.py
@@ -9,14 +9,17 @@ print(myroot)
 print(myroot[0])
 print(myroot[0][0])
 print("\n")
+
 #Print value of an elements
 print("***Value of an elements***")  
 print(myroot[0][0].text)
 print("\n")
+
 #Print all child elements in a root
 print("***All child elements in a root***")
 for c in myroot[0]:
     print(c.text)
+
 #Print all name in food using findall 
 print("***All name in food using findall***")
 for x in myroot.findall("food"):
@@ -24,6 +27,14 @@ for x in myroot.findall("food"):
     price=x.find("price").text
     calories=x.find("calories").text
     print(name,price,calories)
+
+#Change description by adding additional text
+for x in myroot.iter("description"):
+    a=str(x.text)+"+newly added"
+    x.text=str(a)
+    x.set("updated","yes")
+
+mydoc.write("XML/sample1.xml")
 
 
 
